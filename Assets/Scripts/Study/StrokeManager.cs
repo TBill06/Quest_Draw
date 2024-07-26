cf08d101907c691d5720ad15b8b978d7d388991d
jayhendy <jehender94@gmail.com>
2024-07-26 16:00:11 -0230

    added a white background

--- Assets/Scripts/Study/StrokeManager.cs
+++ Assets/Scripts/Study/StrokeManager.cs
@@ -9,7 +9,7 @@ using Unity.ProceduralTube;
 public class StrokeManager : MonoBehaviour
 {
 
-
+    public GameObject background;
     public List<Stroke> strokes;
     
     private int[] order = {0,1,2,3,4,5,6,7,8,9,10,11};
@@ -25,10 +25,12 @@ public class StrokeManager : MonoBehaviour
     }
 
     public void ShowStroke() {
+        background.SetActive(true);
         strokes[order[position]].gameObject.SetActive(true);
     }
 
     public void HideStroke() {
+        background.SetActive(false);
         strokes[order[position]].gameObject.SetActive(false);
     }
 
